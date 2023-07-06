569e0ff7081d4e065ba5fdd336e8620688f890b5
Kinan BSHARA <kbshara@myges.fr>
2023-07-06 10:55:40 +0200

    create dockerfile for each service

--- user-service/src/main.ts
+++ user-service/src/main.ts
@@ -1,11 +1,12 @@
-import {NestFactory} from '@nestjs/core';
-import {AppModule} from './app.module';
 import {MicroserviceOptions, Transport} from '@nestjs/microservices';
 import {UsersValidationPipe} from "./users/users.pipe";
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 import {Logger} from 'nestjs-pino';
 import {join} from 'path'
+import * as process from "process";
 
-
+console.log("env ", process.env.USER_SERVICE_APP_URL, process.env.USER_SERVICE_APP_PORT);
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
       AppModule,
@@ -14,10 +15,11 @@ async function bootstrap() {
           options: {
               package: 'users',
               protoPath: join(__dirname.concat('/users/users.proto')),
-              url: '0.0.0.0:3000'
+              url: `${process.env.USER_SERVICE_APP_URL}:${process.env.USER_SERVICE_APP_PORT}`,
           }
       }
   );
+
   app.useGlobalPipes(new UsersValidationPipe());
   app.useLogger(app.get(Logger));
   await app.listen();
